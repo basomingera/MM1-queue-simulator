83b928375eb847269ebc0eb5f69f633951cbb119
Rob <basorot@gmail.com>
2018-05-26 14:00:19 +0900

    FIXED inconsistent naming

--- MM1.py
+++ MM1.py
@@ -27,7 +27,7 @@ class Job:
     def __init__(self, arrival_time, job_id, job_size):
         self.job_id = job_id
         self.arrival_time = arrival_time
-        self.service_time = random.expovariate(1/job_size)
+        self.service_time = random.expovariate(1 / job_size)
         self.service_start_time = 0
         self.service_end_time = 0
         self.job_delay_time = 0
@@ -68,7 +68,7 @@ class System:
                     this_job.status = 3
                     if DEBUG:
                         print("Time: " + str(this_job.service_end_time) + "secs \t\tJod Id: " + str(
-                        this_job.job_id) + " Finished processing , queue size is: " + str(len(self.queue_list)-1))
+                            this_job.job_id) + " Finished processing , queue size is: " + str(len(self.queue_list) - 1))
                     self.queue_list.remove(this_job)
                     self.queue_summary_over_time[current_time] = len(self.queue_list)
                     finished_jobs.append(this_job)
@@ -79,7 +79,7 @@ class System:
                 this_job.status = 3
                 if DEBUG:
                     print("Time: " + str(this_job.service_end_time) + "secs \t\tJod Id: " + str(
-                    this_job.job_id) + " Finished processing, queue size is: " + str(len(self.queue_list)-1))
+                        this_job.job_id) + " Finished processing, queue size is: " + str(len(self.queue_list) - 1))
                 self.queue_list.remove(this_job)
                 self.queue_summary_over_time[current_time] = len(self.queue_list)
                 finished_jobs.append(this_job)
@@ -92,7 +92,7 @@ class System:
 
             if DEBUG:
                 print("Time: " + str(current_time) + "secs \t\tJod Id: " + str(
-                the_new_job.job_id) + " Entered system, system job size is: " + str(len(self.queue_list)))
+                    the_new_job.job_id) + " Entered system, system job size is: " + str(len(self.queue_list)))
             the_new_job.status = 1
 
     def finalize_jobs(self):
@@ -112,20 +112,21 @@ class System:
             elif this_job.status < 2:
                 if DEBUG:
                     print("Time: " + str(this_job.service_start_time) + "secs \t\tJod Id: " + str(
-                    this_job.job_id) + " Started processing ..... ")
+                        this_job.job_id) + " Started processing ..... ")
                 self.queue_summary_over_time[this_job.service_end_time] = len(self.queue_list)
                 this_job.status = 2
 
                 this_job.status = 3
                 if DEBUG:
                     print("Time: " + str(this_job.service_end_time) + "secs \t\tJod Id: " + str(
-                        this_job.job_id) + " Finished processing , queue size is: " + str(len(self.queue_list)-1))
+                        this_job.job_id) + " Finished processing , queue size is: " + str(len(self.queue_list) - 1))
                 self.queue_list.remove(this_job)
                 self.queue_summary_over_time[this_job.service_end_time] = len(self.queue_list)
                 if this_job.service_end_time > current_time:
-                        current_time = this_job.service_end_time
+                    current_time = this_job.service_end_time
 
         print("Time: " + str(current_time) + "secs End of last job in the System\nSimulation summary:")
+
 
 class Simulator:
     def __init__(self, arrival_rate, service_rate):
@@ -135,12 +136,12 @@ class Simulator:
     def run(self, simulation_time):
         print("\nTime: 0 sec, Simulation starts for λ=" + str(self.arrival_rate))
         current_time = random.expovariate(self.arrival_rate)
-        jobs = {}  # map of id:job
+        this_jobs = {}  # map of id:job
         job_id = 1
 
         while current_time <= simulation_time:
             new_job = Job(current_time, job_id, self.system.service_rate)
-            jobs[job_id] = new_job
+            this_jobs[job_id] = new_job
             new_job.add_and_process_job_queue(self.system)
 
             self.system.handle_jobs(new_job)
@@ -148,8 +149,8 @@ class Simulator:
             job_id += 1
 
         self.system.finalize_jobs()
-        print("Total jobs: " + str(len(jobs)))
-        return jobs
+        print("Total jobs: " + str(len(this_jobs)))
+        return this_jobs
 
 
 def plot_simulation_delay_time_per_job(jobs, arrival_rate, sumarize):
@@ -157,71 +158,73 @@ def plot_simulation_delay_time_per_job(jobs, arrival_rate, sumarize):
 
     simulation_data = [job_ids, [jobs[job_id].job_delay_time for job_id in jobs]]
 
-    simulation_delay_avg = sum(simulation_data[1])/len(simulation_data[1])
+    simulation_delay_avg = sum(simulation_data[1]) / len(simulation_data[1])
     print("Average delay per job: " + str(simulation_delay_avg))
     simulation_data_delay_averages = [job_ids, [simulation_delay_avg for job_id in jobs]]
 
     theoretical_data = [job_ids, [1 / ((1 / JOB_SIZE) - arrival_rate) for job_id in jobs]]
 
     plt.figure(" Figure for lambda=" + str(arrival_rate))
-    axis = plt.subplot()
-    axis.step(simulation_data[0], simulation_data[1], label='Simulation delay time per job id')
-    axis.step(simulation_data_delay_averages[0], simulation_data_delay_averages[1], label='Simulation E[T]')
-    axis.step(theoretical_data[0], theoretical_data[1], label='Theoretical E[T]')
-    axis.set_xlabel('Job Id')
-    axis.set_ylabel('Delay Time (secs)')
-    axis.legend()
-    axis.set_title("Delay time M/M/1 λ=" + str(arrival_rate) + " Simulation time: " + str(TOTAL_SIMULATION_TIME) +" secs")
-    # plt.show()
+    this_axis = plt.subplot()
+    this_axis.step(simulation_data[0], simulation_data[1], label='Simulation delay time per job id')
+    this_axis.step(simulation_data_delay_averages[0], simulation_data_delay_averages[1], label='Simulation E[T]')
+    this_axis.step(theoretical_data[0], theoretical_data[1], label='Theoretical E[T]')
+    this_axis.set_xlabel('Job Id')
+    this_axis.set_ylabel('Delay Time (secs)')
+    this_axis.legend()
+    this_axis.set_title(
+        "Delay time M/M/1 λ=" + str(arrival_rate) + " Simulation time: " + str(TOTAL_SIMULATION_TIME) + " secs")
 
     sumarize[arrival_rate] = [simulation_delay_avg, 1 / ((1 / JOB_SIZE) - arrival_rate)]
+
 
 if __name__ == '__main__':
     summary_results = {}
     for this_lambda in LAMBDAS:
         simulator = Simulator(this_lambda, JOB_SIZE)
-        jobs = simulator.run(TOTAL_SIMULATION_TIME)
+        the_jobs = simulator.run(TOTAL_SIMULATION_TIME)
 
-        plot_simulation_delay_time_per_job(jobs, this_lambda, summary_results)
+        plot_simulation_delay_time_per_job(the_jobs, this_lambda, summary_results)
 
         # write data to csv file
         if DEBUG:
             outDataFileName = "./jobs_" + str(this_lambda) + ".csv"
             outfile = open(outDataFileName, "w")
             writer = csv.writer(outfile)
-            header = ['id', 'arrival', 'service time start', 'service time end', 'total delay', 'Service time', 'queue time']
+            header = ['id', 'arrival', 'service time start', 'service time end', 'total delay', 'Service time',
+                      'queue time']
             writer.writerow(header)
 
-            for job in jobs:
+            for job in the_jobs:
                 job_data = []
-                job_data.append(jobs[job].job_id)
-                job_data.append(jobs[job].arrival_time)
-                job_data.append(jobs[job].service_start_time)
-                job_data.append(jobs[job].service_end_time)
-                job_data.append(jobs[job].job_delay_time)
-                job_data.append(jobs[job].service_time)
-                job_data.append(jobs[job].queue_time)
+                job_data.append(the_jobs[job].job_id)
+                job_data.append(the_jobs[job].arrival_time)
+                job_data.append(the_jobs[job].service_start_time)
+                job_data.append(the_jobs[job].service_end_time)
+                job_data.append(the_jobs[job].job_delay_time)
+                job_data.append(the_jobs[job].service_time)
+                job_data.append(the_jobs[job].queue_time)
                 writer.writerow(job_data)
             outfile.close()
 
     lamdas = [lamda for lamda in summary_results]
 
-    simulation_data = [lamdas, [summary_results[lamda][0] for lamda in lamdas]]
-    theoretical_data = [lamdas, [summary_results[lamda][1] for lamda in lamdas]]
+    the_simulation_data = [lamdas, [summary_results[lamda][0] for lamda in lamdas]]
+    the_theoretical_data = [lamdas, [summary_results[lamda][1] for lamda in lamdas]]
 
     plt.figure(" Comparison")
     axis = plt.subplot()
 
-    plt.plot(simulation_data[0], simulation_data[1], 'b--')
-    plt.plot(simulation_data[0], simulation_data[1], 'bs', label='Simulation delay time')
+    plt.plot(the_simulation_data[0], the_simulation_data[1], 'b--')
+    plt.plot(the_simulation_data[0], the_simulation_data[1], 'bs', label='Simulation delay time')
 
-    plt.plot(theoretical_data[0], theoretical_data[1],  'g--')
-    plt.plot(theoretical_data[0], theoretical_data[1],  'go', label='Theoretical E[T]')
+    plt.plot(the_theoretical_data[0], the_theoretical_data[1], 'g--')
+    plt.plot(the_theoretical_data[0], the_theoretical_data[1], 'go', label='Theoretical E[T]')
 
     axis.set_xlabel('Lambda Value')
     axis.set_ylabel('Delay Time (secs)')
     axis.legend()
-    axis.set_title(
-        "Simulation Vs steady-state: Avg Delay time on M/M/1 " + ", Simulation time: " + str(TOTAL_SIMULATION_TIME) + "secs")
+    axis.set_title("Simulation Vs steady-state: Avg Delay time on M/M/1 " + ", Simulation time: " + str(
+        TOTAL_SIMULATION_TIME) + "secs")
     plt.show()
     plt.close()
